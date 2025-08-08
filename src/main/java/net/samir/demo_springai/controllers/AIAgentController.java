c80ed630e9d571b921598839564770764b120497
Samireddebbarhi <samireddebbarhi29@gmail.com>
2025-08-08 14:47:14 +0100

    adding fewShot Examples

--- src/main/java/net/samir/demo_springai/controllers/AIAgentController.java
+++ src/main/java/net/samir/demo_springai/controllers/AIAgentController.java
@@ -1,9 +1,16 @@
 package net.samir.demo_springai.controllers;
 
 
+import org.jboss.logging.Messages;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
 
+import java.util.List;
+
 @RestController
 public class AIAgentController {
     private ChatClient chatClient;
     private static final Logger logger = LoggerFactory.getLogger(AIAgentController.class);
 
-    public AIAgentController(ChatClient.Builder builder) {
-        this.chatClient = builder.defaultAdvisors(new SimpleLoggerAdvisor()).build();
+    public AIAgentController(ChatClient.Builder builder, ChatMemory memory) {
+        this.chatClient = builder.defaultAdvisors(new SimpleLoggerAdvisor()).defaultAdvisors(MessageChatMemoryAdvisor.builder(memory).build()).build();
 
     }
 
     @GetMapping("/chat")
     public ResponseEntity<String> askLLM(@RequestParam("query") String query) {
         try {
+            List<Message> examples = List.of(new UserMessage("2+7"),new AssistantMessage("9"));
+
             String response = chatClient.prompt()
                     .user(query)
+                    .system("Always respond with UPPERCASE0")
+                    .messages(examples)
                     .call()
                     .content();
             return ResponseEntity.ok(response);
